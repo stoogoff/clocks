a64950927147b0c45240257516271fcf87b386e6
Stoo <stoo.goff@gmail.com>
2025-01-17 23:32:16 +0000

    Removed pouch

--- src/main.js
+++ src/main.js
@@ -1,30 +1,13 @@
 
-import { DIRECTIVES_LOG_KEY } from 'quick/reactive/config.js'
 import { setLogger, ConsoleLogger, LOG_LEVEL_LOG, LOG_LEVEL_INFO } from 'quick/utils/logger.js'
-
 import { directives } from 'quick'
 import ClockAdd from '/modules/clock/add.js'
 import ClockManager from '/modules/clock/manager.js'
 import ClockView from '/modules/clock/view.js'
 import { clockStore } from '/modules/clock/store.js'
 import { CLOCK_LOG_KEY } from '/modules/clock/logger.js'
-import { POUCH_LOG_KEY, getAllClocks, removeClock } from '/migrations/pouch.js'
 
-//setLogger(DIRECTIVES_LOG_KEY, ConsoleLogger, LOG_LEVEL_LOG)
 setLogger(CLOCK_LOG_KEY, ConsoleLogger, LOG_LEVEL_INFO)
-setLogger(POUCH_LOG_KEY, ConsoleLogger, LOG_LEVEL_INFO)
-
-async function migrateFromPouch() {
-	const all = await getAllClocks()
-
-	all.forEach(async clock => {
-		clockStore.add(clock)
-
-		await removeClock(clock)
-	})
-}
-
-migrateFromPouch()
 
 // register custom component
 directives.register('colour', (node, property, scope) => {
